91a46f1e7933bf9020183a44943108c771f1e95e
razeg04 <63110480+razeg04@users.noreply.github.com>
2021-04-05 21:27:22 -0700

    mengimport class Mahasiswa dari package model

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import koneksi.Database;
+import model.Mahasiswa;
 
 public class Main{
 public static void main(String[]args){
 System.out.println("Ini program utama");
 
 Database.hubungkan();
+Mahasiswa.tampilkanInfo();
 }
 }
---
+++ model/Mahasiswa.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Mahasiswa{
+public static void tampilkanInfo(){
+System.out.println("Nama : Putra");
+System.out.println("IPK : 3.99");
+}
+}
